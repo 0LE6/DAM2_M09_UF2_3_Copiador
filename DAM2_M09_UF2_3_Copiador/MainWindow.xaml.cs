a50c0e0042bf2f0a0ce2e51f651072708c010b07
Oleg <kharenkoleg@gmail.com>
2024-02-08 17:53:20 +0100

    <<

--- DAM2_M09_UF2_3_Copiador/MainWindow.xaml.cs
+++ DAM2_M09_UF2_3_Copiador/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -28,12 +29,22 @@ namespace DAM2_M09_UF2_3_Copiador
 
         private void btnOrigen_Click(object sender, RoutedEventArgs e)
         {
-            var ofd = ();
+            // var ofd = OpenFolderDialog(); .net 8
+
+            var ofd = new FolderBrowserDialog(); // doble click en el projecto y ponemos a true el useWindowsForms
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.tbOrigen.Text = ofd.SelectedPath; // para seleccionar un directorio
+            }
         }
 
         private void btnDesti_Click(object sender, RoutedEventArgs e)
         {
-
+            var ofd = new FolderBrowserDialog(); // doble click en el projecto y ponemos a true el useWindowsForms
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.tbDesti.Text = ofd.SelectedPath; // para seleccionar un directorio
+            }
         }
 
         private void btnCopia_Click(object sender, RoutedEventArgs e)
